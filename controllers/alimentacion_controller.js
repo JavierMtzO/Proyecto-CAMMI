f87d3f1896a46964e4b9ba2c04782a6cec546bc5
a01173530 <A01173530@itesm.mx>
2021-10-31 14:56:06 -0600

    Controlador de alimentos

--- controllers/alimentacion_controller.js
+++ controllers/alimentacion_controller.js
@@ -1,16 +1,16 @@
 
 const Alimentacion = require('../models/alimentacion');
-/*
-exports.getNuevoMigrante=(request, response, next) => {
+
+exports.getNuevoAlimentacion=(request, response, next) => {
 
 	response.render('registro-Migrante');/*,{
          //permisos: request.session.permiso
     });*/
 
-/*}
+}
 
 
-exports.postNuevoMigrante=(request, response, next) => {
+exports.postNuevoAlimentacion=(request, response, next) => {
     console.log(request.body);
     const persona = new Persona(request.body.Nombre, request.body.Edad, request.body.Rango_de_Edad,request.body.Genero,request.body.Nacionalidad, request.body.Discapacidad,request.body.Contacto,request.body.Lgbt,request.body.Dispositivo_propio,request.body.Rfc,request.body.Nss,request.body.Asesoria,request.body.Acompanado);
     persona.save()
@@ -21,4 +21,4 @@ exports.postNuevoMigrante=(request, response, next) => {
             console.log(err);
             response.redirect('/migrante/migrante');    
        });
- }*/
+ }

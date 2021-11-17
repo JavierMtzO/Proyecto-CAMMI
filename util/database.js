6f1f3acf29f404203ba75c5db7d582e51695376a
a01173530 <A01173530@itesm.mx>
2021-11-16 22:28:33 -0600

    normalidad

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'CAMMI',
-    password: '', //root
+    password: 'root', //root
 });
 
 module.exports = pool.promise();

6004e1d1facafde5f70cd4b54865a8559bfe4d21
a01173530 <A01173530@itesm.mx>
2021-11-29 21:47:46 -0600

    root base de datos

--- util/database.js
+++ util/database.js
@@ -4,7 +4,7 @@ const pool = mysql.createPool({
     host: 'localhost',
     user: 'root',
     database: 'CAMMI',
-    password: 'root', //root
+    password: '', //root
 });
 
 module.exports = pool.promise();
